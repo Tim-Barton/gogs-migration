cf89add1d22dc28988e52346008de2a4a3bded04
Tim Barton <timothyebarton@gmail.com>
2018-04-17 15:22:42 +1000

    Determine project to migrate (ignore projects that already exist)

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 #! /usr/bin/python
 
 import argparse
+import sys
 from gitlabclient import GitlabClient
 from gogsclient import GogsClient
 
-
-def migrateProject(args):
-    pass
-    #create project on gogs (use migrate util?)
-    #if wiki enabled, create wiki, migrate wiki
 
 parser = argparse.ArgumentParser("Migrate projects (repos & Wikis) from Gitlab to Gogs")
 parser.add_argument('-glp', '--gitlabpat', help="The Personal Access Token (PAT) for the Gitlab user")
@@ -16,15 +12,27 @@ parser.add_argument('-gl', '--gitlaburl', help="The base url of the gitlab insta
 parser.add_argument('-go', '--gogsurl', help="The base url of the gogs instance")
 parser.add_argument('-goo', '--gogsowner', help="The owner on gogs that all the projects to be migrated to")
 parser.add_argument('-gop', '--gogspat', help="The Personal Access Token (PAT) for the Gogs user")
+parser.add_argument('-tmp', "--tempdir", default="/tmp", help="Directory to use for temporary storage of artifacts during migration")
 args = parser.parse_args()
 
 gitlab = GitlabClient(args)
-
-projects = gitlab.getAllGroupProjects()
-
-#print(projects)
-    
 gogs = GogsClient(args)
 
-if not gogs.checkIfOwnerExists(args.gogsowner):
+if args.gogsowner is not None and not gogs.ownerExists(args.gogsowner):
     print("You need to create organisation/user {} in order to proceed with migration".format(args.gogsowner))
+    sys.exit(1)
+
+
+gitlabProjects = gitlab.getAllGroupProjects()
+gogsProjects = gogs.getAllProjectsForOwner(args.gogsowner)
+gogsProjectNames = [gogs.transformShortName(project["full_name"]) for project in gogsProjects ]
+migrateProjects = [project for project in gitlabProjects if project["name"] not in gogsProjectNames ]
+
+print(migrateProjects)
+
+for project in migrateProjects:
+    #create gogs project with name
+    #migrate git repo
+    #migrate issues
+    #migrate wiki
+    pass
